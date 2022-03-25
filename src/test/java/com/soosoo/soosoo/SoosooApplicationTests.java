409f97dfa1205fc0ba31928f568b7de2f42ae1b2
kor-kms <minsuk0704@naver.com>
2022-03-26 01:01:42 +0900

    Fix: test 주석처리

--- src/test/java/com/soosoo/soosoo/SoosooApplicationTests.java
+++ src/test/java/com/soosoo/soosoo/SoosooApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SoosooApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
