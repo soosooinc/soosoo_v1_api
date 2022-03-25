5283e46e1288c9c76fe1a2acb600c6592ecaf73d
kor-kms <minsuk0704@naver.com>
2022-03-26 00:36:25 +0900

    Feat: make first project

---
+++ src/test/java/com/soosoo/soosoo/SoosooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soosoo.soosoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoosooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
