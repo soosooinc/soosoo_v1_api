61dc2dbd3583cc85778672211a0e817b37192223
JINYOUNGCHOI <cjy9249@tmon.co.kr>
2022-03-30 20:43:37 +0900

    feat : kafka config

--- src/main/java/com/soosoo/soosoo/SoosooApplication.java
+++ src/main/java/com/soosoo/soosoo/SoosooApplication.java
@@ -1,13 +1,29 @@
 package com.soosoo.soosoo;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.Properties;
 
 @SpringBootApplication
 public class SoosooApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(SoosooApplication.class, args);
+
+		Properties configs = new Properties();
+		configs.put("bootstrap.servers", "localhost:9092");
+		configs.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		configs.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(configs);
+		ProducerRecord record = new ProducerRecord<String,String>("click_log" , "login");
+		producer.send(record);
+		producer.close();
+
 	}
 
 }
