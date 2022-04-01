4fda6b41d473670c8ad0b04340a76310e22e0ea9
suzi <pc_suzi@daum.net>
2022-04-01 22:26:30 +0900

    Feat: add kindergarten entity

---
+++ src/main/java/com/soosoo/soosoo/domain/entity/Kindergarten.java
@@ -0,0 +1,38 @@
+package com.soosoo.soosoo.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_kindergarten")
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Kindergarten extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Integer kindergartenId;
+
+    @Column(name = "user_id_fk", nullable = false, columnDefinition = "TEXT")
+    private Integer userId;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String address;
+
+    @Column
+    private Short phone;
+
+}
---
+++ src/main/java/com/soosoo/soosoo/service/kindergarten/facade/kindergartenFacade.java
@@ -0,0 +1,11 @@
+package com.soosoo.soosoo.service.kindergarten.facade;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class kindergartenFacade {
+
+}
+
