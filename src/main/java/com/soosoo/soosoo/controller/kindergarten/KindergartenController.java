7925efa2b8f31c7ad1768c41661ad57bb1bbde36
suzi <pc_suzi@daum.net>
2022-04-01 22:33:52 +0900

    Delete: delete annotation

--- src/main/java/com/soosoo/soosoo/controller/kindergarten/KindergartenController.java
+++ src/main/java/com/soosoo/soosoo/controller/kindergarten/KindergartenController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("kindergarten") //큰따옴편만 , url 주소
+@RequestMapping("kindergarten")
 @RequiredArgsConstructor
 public class KindergartenController {
 }
