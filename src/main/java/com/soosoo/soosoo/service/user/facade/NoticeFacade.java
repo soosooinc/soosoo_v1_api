435e5bd0e4aaaffd25e9c6347a407986622bbb55
jennie <leeje2432@42maru.com>
2022-04-05 10:59:43 +0900

    Feat: add notice facade

---
+++ src/main/java/com/soosoo/soosoo/service/user/facade/NoticeFacade.java
@@ -0,0 +1,9 @@
+package com.soosoo.soosoo.service.user.facade;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeFacade {
+}
