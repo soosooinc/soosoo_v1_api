08b4a71aa44761b0d5143c6956c273282ccb66d9
최유 <comecomi_i@choeyuui-MacBookAir.local>
2022-04-05 21:51:33 +0900

    Feat: add imageFacade

---
+++ src/main/java/com/soosoo/soosoo/service/image/facade/ImageFacade.java
@@ -0,0 +1,9 @@
+package com.soosoo.soosoo.service.image.facade;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ImageFacade {
+}
