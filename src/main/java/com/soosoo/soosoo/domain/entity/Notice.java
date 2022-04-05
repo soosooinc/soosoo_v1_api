5afaf930e3c305e37a3d3b793420c72107308085
jennie <leeje2432@42maru.com>
2022-04-05 10:18:46 +0900

    Feat: add Notice Entity

---
+++ src/main/java/com/soosoo/soosoo/domain/entity/Notice.java
@@ -0,0 +1,41 @@
+package com.soosoo.soosoo.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_notice")
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notice extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Integer noticeId;
+
+    @Column(name = "image_id_fk")
+    private Integer imageId;
+
+    @Column(nullable = false)
+    private Integer topId;
+
+    @Column(nullable = false, columnDefinition = "smallint")
+    private Short type;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String mainText;
+
+    @Column(columnDefinition = "TEXT")
+    private String subText;
+}
