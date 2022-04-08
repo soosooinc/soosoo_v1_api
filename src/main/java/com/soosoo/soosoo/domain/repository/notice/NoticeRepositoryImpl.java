c07b4e74777a15b91f2e20c30e42d6674a2a96fa
kor-kms <68898456+kor-kms@users.noreply.github.com>
2022-04-09 01:34:56 +0900

    Feat: add notice api (#9)

---
+++ src/main/java/com/soosoo/soosoo/domain/repository/notice/NoticeRepositoryImpl.java
@@ -0,0 +1,9 @@
+package com.soosoo.soosoo.domain.repository.notice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class NoticeRepositoryImpl implements NoticeRepositoryCustom{
+}
