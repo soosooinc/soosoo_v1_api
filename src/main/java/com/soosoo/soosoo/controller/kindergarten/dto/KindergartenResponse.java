536d6f58df4dc3cd6295583665fe157e42b60d50
kor-kms <minsuk0704@naver.com>
2022-04-10 14:34:16 +0900

    Feat: change annotation

--- src/main/java/com/soosoo/soosoo/controller/kindergarten/dto/KindergartenResponse.java
+++ src/main/java/com/soosoo/soosoo/controller/kindergarten/dto/KindergartenResponse.java
@@ -1,22 +1,16 @@
 package com.soosoo.soosoo.controller.kindergarten.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 public class KindergartenResponse {
     @Getter
     @Builder
-    @NoArgsConstructor
+    @AllArgsConstructor
     public static class TeacherInfoResponse {
         private Integer userId;
         private String name;
         private String imageUrl;
-
-        public TeacherInfoResponse(Integer userId, String name, String imageUrl) {
-            this.userId = userId;
-            this.name = name;
-            this.imageUrl = imageUrl;
-        }
     }
 }
