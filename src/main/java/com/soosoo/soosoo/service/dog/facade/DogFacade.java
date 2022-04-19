998c31a5ccae12b6b5b8d4da3e3a02d089a46fac
최유 <comecomi_i@choeyuui-MacBookAir.local>
2022-04-20 02:44:19 +0900

    Feat: add get DogInfo API

--- src/main/java/com/soosoo/soosoo/service/dog/facade/DogFacade.java
+++ src/main/java/com/soosoo/soosoo/service/dog/facade/DogFacade.java
@@ -1,9 +1,19 @@
 package com.soosoo.soosoo.service.dog.facade;
 
+import com.soosoo.soosoo.service.dog.GetDogInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class DogFacade {
+    private final GetDogInfoService getDogInfoService;
+
+    @Transactional(readOnly = true)
+    public String getDogInfo(int dogId) {
+        return getDogInfoService.getDogInfo(dogId);
+    }
 }
+
+
