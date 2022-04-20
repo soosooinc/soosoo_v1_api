8ed83758f83e03b1b3a06789e2e2211929f9339b
kor-kms <minsuk0704@naver.com>
2022-04-20 22:08:39 +0900

    Feat: delete annotation

--- src/main/java/com/soosoo/soosoo/domain/repository/dog/DogRepository.java
+++ src/main/java/com/soosoo/soosoo/domain/repository/dog/DogRepository.java
@@ -6,19 +6,3 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface DogRepository extends JpaRepository<Dog, Integer>, DogRepositoryCustom {
     Dog findByDogId(int dogId);
 }
-
-
-/*
-package com.soosoo.soosoo.domain.repository.image;
-
-import com.soosoo.soosoo.domain.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Integer>, ImageRepositoryCustom {
-    Image findByImageId(int imageId);
-}
-public interface NoticeRepository extends JpaRepository<Notice, Integer>, NoticeRepositoryCustom {
-    Notice findByNoticeId(int noticeId);
-}
-
- */
