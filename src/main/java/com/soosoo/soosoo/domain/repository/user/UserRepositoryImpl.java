6f2e3736159b742c4a4bd643fd4c79d7400ca4fe
jennie <leeje2432@42maru.com>
2022-04-27 22:02:56 +0900

    Feat: add userinfo querydsl

--- src/main/java/com/soosoo/soosoo/domain/repository/user/UserRepositoryImpl.java
+++ src/main/java/com/soosoo/soosoo/domain/repository/user/UserRepositoryImpl.java
@@ -1,17 +1,14 @@
 package com.soosoo.soosoo.domain.repository.user;
 
 import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.soosoo.soosoo.controller.user.dto.UserResponse;
-import com.soosoo.soosoo.domain.entity.QImage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-import static com.soosoo.soosoo.domain.entity.QImage.image;
 import static com.soosoo.soosoo.domain.entity.QUser.user;
+import static com.soosoo.soosoo.domain.entity.QImage.image;
+import static com.soosoo.soosoo.domain.entity.QKindergarten.kindergarten;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,6 +17,15 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
 
     @Override
     public UserResponse.UserInfoResponse getUserInfo(int userId) {
-        return null;
+        return jpaQueryFactory
+                .select(Projections.constructor(UserResponse.UserInfoResponse.class,
+                        user.userId, kindergarten.name, image.imageUrl,
+                        user.id, user.name, user.type, user.phone, user.email
+                        ))
+                .from(image)
+                .innerJoin(user).on(image.imageId.eq(user.imageId))
+                .innerJoin(kindergarten).on(kindergarten.kindergartenId.eq(user.kindergartenId))
+                .where(user.userId.eq(userId))
+                .fetchOne();
     }
 }
