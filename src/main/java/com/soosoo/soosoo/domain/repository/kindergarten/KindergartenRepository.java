fff7618eda77f2233d4c3753eda19966227031f4
jennie <leeje2432@42maru.com>
2022-04-27 22:05:46 +0900

    Delete: remove unused imports

--- src/main/java/com/soosoo/soosoo/domain/repository/kindergarten/KindergartenRepository.java
+++ src/main/java/com/soosoo/soosoo/domain/repository/kindergarten/KindergartenRepository.java
@@ -1,8 +1,6 @@
 package com.soosoo.soosoo.domain.repository.kindergarten;
 
 import com.soosoo.soosoo.domain.entity.Kindergarten;
-import com.soosoo.soosoo.domain.entity.User;
-import com.soosoo.soosoo.domain.repository.user.UserRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface KindergartenRepository extends JpaRepository<Kindergarten,Integer>, KindergartenRepositoryCustom{
--- src/main/java/com/soosoo/soosoo/domain/repository/user/UserRepositoryCustom.java
+++ src/main/java/com/soosoo/soosoo/domain/repository/user/UserRepositoryCustom.java
@@ -2,8 +2,6 @@ package com.soosoo.soosoo.domain.repository.user;
 
 import com.soosoo.soosoo.controller.user.dto.UserResponse;
 
-import java.util.List;
-
 public interface UserRepositoryCustom {
     UserResponse.UserInfoResponse getUserInfo(int userId);
 }
