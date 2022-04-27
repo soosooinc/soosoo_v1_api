a052ba49bd3b556bab5b4539c9a0dedf4326e6d3
jennie <leeje2432@42maru.com>
2022-04-27 18:02:37 +0900

    Feat: add UserInfo Response

--- src/main/java/com/soosoo/soosoo/domain/repository/user/UserRepositoryCustom.java
+++ src/main/java/com/soosoo/soosoo/domain/repository/user/UserRepositoryCustom.java
@@ -1,4 +1,9 @@
 package com.soosoo.soosoo.domain.repository.user;
 
+import com.soosoo.soosoo.controller.user.dto.UserResponse;
+
+import java.util.List;
+
 public interface UserRepositoryCustom {
+    UserResponse.UserInfoResponse getUserInfo(int userId);
 }
--- src/main/java/com/soosoo/soosoo/domain/repository/user/UserRepositoryImpl.java
+++ src/main/java/com/soosoo/soosoo/domain/repository/user/UserRepositoryImpl.java
@@ -1,9 +1,25 @@
 package com.soosoo.soosoo.domain.repository.user;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.soosoo.soosoo.controller.user.dto.UserResponse;
+import com.soosoo.soosoo.domain.entity.QImage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.soosoo.soosoo.domain.entity.QImage.image;
+import static com.soosoo.soosoo.domain.entity.QUser.user;
 
 @Repository
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepositoryCustom {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public UserResponse.UserInfoResponse getUserInfo(int userId) {
+        return null;
+    }
 }
