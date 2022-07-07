1a9d3083468859026564835a120d5d03002dc732
suzi <pc_suzi@daum.net>
2022-07-08 00:06:06 +0900

    Feat: add updateKindergartenInfo Repository

--- src/main/java/com/soosoo/soosoo/domain/repository/kindergarten/KindergartenRepositoryCustom.java
+++ src/main/java/com/soosoo/soosoo/domain/repository/kindergarten/KindergartenRepositoryCustom.java
@@ -9,4 +9,5 @@ public interface KindergartenRepositoryCustom {
     List<KindergartenResponse.TeacherInfoResponse> getTeacherInfo(int kindergartenId);
     KindergartenJoinImageForResponse getKindergartenInfo(int kindergartenId);
     long addTeacher(int kindergartenId, int userId);
+    long updateKindergartenInfo(KindergartenJoinImageForResponse kindergartenJoinImage);
 }
